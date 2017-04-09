53e5a96541f7240b19ef4c6a1033aed5a3556295
leibro <cs4814751@gmail.com>
2017-04-09 14:57:30 +0800

    添加Logback配置

--- src/test/java/com/leibro/service/SeckillSerivceTest.java
+++ src/test/java/com/leibro/service/SeckillSerivceTest.java
@@ -2,6 +2,9 @@ package com.leibro.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,8 +18,14 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:Spring/spring-dao.xml","classpath:Spring/spring-service.xml"})
 public class SeckillSerivceTest {
+    @Autowired
+    private SeckillSerivce seckillSerivce;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Test
     public void getSeckillList() throws Exception {
+
     }
 
     @Test
