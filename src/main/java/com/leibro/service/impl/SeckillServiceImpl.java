74f58b3924b425a4879ea8be68fdf3b265b0a6f4
leibro <cs4814751@gmail.com>
2017-04-09 13:01:34 +0800

    加入声明式注解事务控制

--- src/main/java/com/leibro/service/impl/SeckillServiceImpl.java
+++ src/main/java/com/leibro/service/impl/SeckillServiceImpl.java
@@ -14,6 +14,7 @@ import com.leibro.service.SeckillSerivce;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -59,6 +60,13 @@ public class SeckillServiceImpl implements SeckillSerivce {
         return new Exposer(true,md5,seckillId);
     }
 
+    @Transactional
+    /**
+     * 使用注解控制事务方法的优点：
+     * 1：开发团队达成一致约定，明确标注事务方法的编程风格
+     * 2：保证事务方法的执行时间尽可能短，不要穿插其他网络操作,RPC/HTTP请求或者剥离到事务方法外
+     * 3：不是所有方法都需要事务，如只有一条修改操作，只读操作
+     */
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
         if(md5 == null || !md5.equals(getMD5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
---
+++ src/test/java/com/leibro/service/SeckillSerivceTest.java
@@ -0,0 +1,34 @@
+package com.leibro.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.swing.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by leibro on 2017/4/9.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:Spring/spring-dao.xml","classpath:Spring/spring-service.xml"})
+public class SeckillSerivceTest {
+    @Test
+    public void getSeckillList() throws Exception {
+    }
+
+    @Test
+    public void getById() throws Exception {
+    }
+
+    @Test
+    public void exportSeckillUrl() throws Exception {
+    }
+
+    @Test
+    public void executeSeckill() throws Exception {
+    }
+
+}
